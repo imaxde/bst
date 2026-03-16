3310f4bb22756c8e31cbafbffae6d2fb987ea3a3
Ella Zamyatina <230418248+ellazamyatina@users.noreply.github.com>
2026-03-16 22:37:39 +0300

    Add description for the functions Also made clang format again

--- src/bst.h
+++ src/bst.h
@@ -1,7 +1,21 @@
 #pragma once
 
+#include <stdbool.h>
+
 typedef struct BST BST;
 typedef struct Node Node;
+
+// function for create tree
+BST* bstCreate(void);
+
+// function for inserting nodes
+void bstInsert(BST* tree, int value);
+
+// function check value of the node
+bool bstContains(BST* tree, int value);
+
+// function for free tree
+void bstFree(BST* tree);
 
 // function for recursive tree traversal in order
 void bstInorder(BST* tree);
@@ -11,12 +25,3 @@ void bstPreorder(BST* tree);
 
 // function for recursive tree traversal in postorder
 void bstPostorder(BST* tree);
-#include <stdbool.h>
-
-typedef struct Node Node;
-typedef struct BST BST;
-
-BST* bstCreate(void);
-void bstInsert(BST* tree, int value);
-bool bstContains(BST* tree, int value);
-void bstFree(BST* tree);
