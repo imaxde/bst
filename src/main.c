4256f20bc593d0f77242d61f5554affc0c831e96
Ella Zamyatina <230418248+ellazamyatina@users.noreply.github.com>
2026-03-16 22:09:17 +0300

    Clang format

--- src/main.c
+++ src/main.c
@@ -18,25 +18,25 @@ int main()
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool testBST(void)
-{
-    BST* tree = bstCreate();
-    if (!tree)
-        return false;
+    bool testBST(void)
+    {
+        BST* tree = bstCreate();
+        if (!tree)
+            return false;
 
-    bstInsert(tree, 1);
-    bstInsert(tree, 2);
-    bstInsert(tree, 3);
+        bstInsert(tree, 1);
+        bstInsert(tree, 2);
+        bstInsert(tree, 3);
 
-    bool result = bstContains(tree, 1) && bstContains(tree, 2) && bstContains(tree, 3) && !bstContains(tree, 4);
+        bool result = bstContains(tree, 1) && bstContains(tree, 2) && bstContains(tree, 3) && !bstContains(tree, 4);
 
-    bstFree(tree);
-    return result;
-}
+        bstFree(tree);
+        return result;
+    }
 
-int main(void)
-{
-    if (testBST())
-        return 0;
-    return 1;
-}
+    int main(void)
+    {
+        if (testBST())
+            return 0;
+        return 1;
+    }
